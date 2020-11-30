33453e4116405876cb11ed82c687553712cfcfb8
Alexander <alexandrsuchkov67@gmail.com>
2020-11-30 23:04:26 +0300

    FIX: something

--- Lab_02/main.py
+++ Lab_02/main.py
@@ -23,41 +23,41 @@ def oval_generate(a, b, shift_x, shift_y):
 # Service parameters
 canvas_screen = [1280, 960]
 user_center = [0, 960]
-birb_center = [canvas_screen[0] // 2, canvas_screen[1] // 2]    # Center coordinate
-ent_width = 10              # Width for all entries
-birb_width = 3              # Width for birb
-body_a = 120                # Oval rad by x
-body_b = 65                 # Oval rad by y
-head_r = 40                 # Circle rad
+birb_center = [canvas_screen[0] // 2, canvas_screen[1] // 2]  # Center coordinate
+ent_width = 10  # Width for all entries
+birb_width = 3  # Width for birb
+body_a = 120  # Oval rad by x
+body_b = 65  # Oval rad by y
+head_r = 40  # Circle rad
 
 # Coordinates for birb
 body = oval_generate(body_a, body_b, 0, 0)
 head = oval_generate(head_r, head_r, 85, 90)
 
-beak = [[birb_center[0] - 175, birb_center[1] - 85], 
-        [birb_center[0] - 110, birb_center[1] - 120], 
-        [birb_center[0] - 110, birb_center[1] - 120], 
-        [birb_center[0] - 110, birb_center[1] - 85], 
-        [birb_center[0] - 110, birb_center[1] - 85], 
+beak = [[birb_center[0] - 175, birb_center[1] - 85],
+        [birb_center[0] - 110, birb_center[1] - 120],
+        [birb_center[0] - 110, birb_center[1] - 120],
+        [birb_center[0] - 110, birb_center[1] - 85],
+        [birb_center[0] - 110, birb_center[1] - 85],
         [birb_center[0] - 175, birb_center[1] - 85]]
 
-tail = [[birb_center[0] + 95, birb_center[1] - 20], 
-        [birb_center[0] + 175, birb_center[1] - 25], 
-        [birb_center[0] + 175, birb_center[1] - 25], 
-        [birb_center[0] + 102, birb_center[1] + 13], 
-        [birb_center[0] + 102, birb_center[1] + 13], 
+tail = [[birb_center[0] + 95, birb_center[1] - 20],
+        [birb_center[0] + 175, birb_center[1] - 25],
+        [birb_center[0] + 175, birb_center[1] - 25],
+        [birb_center[0] + 102, birb_center[1] + 13],
+        [birb_center[0] + 102, birb_center[1] + 13],
         [birb_center[0] + 95, birb_center[1] - 20]]
 
-wing = [[birb_center[0] + 30, birb_center[1] + 10], 
+wing = [[birb_center[0] + 30, birb_center[1] + 10],
         [birb_center[0] + 80, birb_center[1] + 10],
-        [birb_center[0] + 80, birb_center[1] + 10], 
+        [birb_center[0] + 80, birb_center[1] + 10],
         [birb_center[0] + 120, birb_center[1] + 100],
-        [birb_center[0] + 120, birb_center[1] + 100], 
+        [birb_center[0] + 120, birb_center[1] + 100],
         [birb_center[0] + 30, birb_center[1] + 10]]
 
-paws = [[birb_center[0] - 35, birb_center[1] + 60], 
-        [birb_center[0] - 70, birb_center[1] + 130], 
-        [birb_center[0] + 10, birb_center[1] + 65], 
+paws = [[birb_center[0] - 35, birb_center[1] + 60],
+        [birb_center[0] - 70, birb_center[1] + 130],
+        [birb_center[0] + 10, birb_center[1] + 65],
         [birb_center[0] + 50, birb_center[1] + 130]]
 
 birb_coords = [beak, head, body, wing, tail, paws]
@@ -96,7 +96,6 @@ def caching():
             undo[i][j].append(birb_coords[i][j][1])
 
 
-
 def scale_figure(figure, c_x, c_y):
     for i in range(len(figure)):
         tmp_x = figure[i][0]
@@ -125,7 +124,6 @@ def rotate_figure(figure, angle):
     return figure
 
 
-
 def scaling():
     global birb_coords
     global birb_center
@@ -142,10 +140,10 @@ def scaling():
     for i in range(len(birb_coords)):
         birb_coords[i] = scale_figure(birb_coords[i], coeff_x, coeff_y)
 
-    #tmp_x = birb_center[0]
-    #tmp_y = birb_center[1]
-    #birb_center[0] = (tmp_x - user_center[0]) * coeff_x + user_center[0]
-    #birb_center[1] = (tmp_y - user_center[1]) * coeff_y + user_center[1]
+    # tmp_x = birb_center[0]
+    # tmp_y = birb_center[1]
+    # birb_center[0] = (tmp_x - user_center[0]) * coeff_x + user_center[0]
+    # birb_center[1] = (tmp_y - user_center[1]) * coeff_y + user_center[1]
 
     cnv_wall.delete(ALL)
     draw_birb()
@@ -166,8 +164,8 @@ def moving():
     for i in range(len(birb_coords)):
         birb_coords[i] = move_figure(birb_coords[i], step_x, step_y)
 
-    #birb_center[0] += step_x
-    #birb_center[1] += step_y
+    # birb_center[0] += step_x
+    # birb_center[1] += step_y
 
     cnv_wall.delete(ALL)
     draw_birb()
@@ -184,20 +182,18 @@ def rotating(side):
     except:
         messagebox.showerror("Error", "Введен некорректный угол поворота")
         return
-    
+
     for i in range(len(birb_coords)):
         birb_coords[i] = rotate_figure(birb_coords[i], angle)
 
-    
-    #tmp_x = birb_center[0] - user_center[0]
-    #tmp_y = birb_center[1] - user_center[1]
+    # tmp_x = birb_center[0] - user_center[0]
+    # tmp_y = birb_center[1] - user_center[1]
 
-    #birb_center[0] = tmp_x * cos(angle) - tmp_y * sin(angle) + user_center[0]
-    #birb_center[1] = tmp_x * sin(angle) + tmp_y * cos(angle) + user_center[1]
+    # birb_center[0] = tmp_x * cos(angle) - tmp_y * sin(angle) + user_center[0]
+    # birb_center[1] = tmp_x * sin(angle) + tmp_y * cos(angle) + user_center[1]
 
     cnv_wall.delete(ALL)
     draw_birb()
-
 
 
 def move_up():
@@ -221,7 +217,7 @@ def move_down():
         messagebox.showerror("Error", "Введено некорректное смещение по Y.")
         return
 
-    #moving(step_x, step_y)
+    # moving(step_x, step_y)
 
 
 def move_right():
@@ -243,8 +239,7 @@ def move_left():
         return
 
     step_y = 0
-    #moving(step_x, step_y)
-
+    # moving(step_x, step_y)
 
 
 def rotate_rigth():
@@ -255,7 +250,6 @@ def rotate_rigth():
 def rotate_left():
     side = -1
     rotating(side)
-
 
 
 def update_center():
@@ -285,7 +279,7 @@ def go_undo():
         for j in range(len(birb_coords[i])):
             birb_coords[i][j][0] = undo[i][j][0]
             birb_coords[i][j][1] = undo[i][j][1]
-    
+
     cnv_wall.delete(ALL)
     draw_birb()
 
@@ -301,7 +295,7 @@ def go_reset():
         for j in range(len(birb_coords[i])):
             birb_coords[i][j][0] = reset[i][j][0]
             birb_coords[i][j][1] = reset[i][j][1]
-    
+
     cnv_wall.delete(ALL)
     draw_birb()
 
@@ -346,7 +340,7 @@ ent_step_x.grid(row=0, column=1, sticky="WESN", padx=5)
 lbl_step_y.grid(row=1, column=0, sticky="WESN", padx=5)
 ent_step_y.grid(row=1, column=1, sticky="WESN", padx=5)
 
-#Buttons
+# Buttons
 btn_move = Button(frm_btn_move, text="Переместить", command=moving)
 btn_move.pack()
 '''
@@ -370,7 +364,6 @@ frm_step.grid(row=0, column=0, sticky='WESN')
 frm_btn_move.grid(row=1, column=0, padx=10, pady=10)
 frm_move.grid(row=1, column=0, sticky='WESN')
 
-
 # Scale_________________________________________________
 frm_scale = LabelFrame(frm_param, text="Масштабирование")
 
@@ -393,9 +386,8 @@ btn_scale.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky='WESN')
 
 frm_scale.grid(row=2, column=0, sticky='WESN')
 
-
 # Rotation_____________________________________
-frm_rot  = LabelFrame(frm_param, text="Поворот")
+frm_rot = LabelFrame(frm_param, text="Поворот")
 frm_angle = Frame(frm_rot)
 frm_btn_rot = Frame(frm_rot)
 
@@ -431,46 +423,45 @@ cnv_wall.pack()
 
 frm_draw.grid(row=0, column=1, sticky='WESN')
 frm_param.grid(row=0, column=0, sticky='WESN')
-    
 
 
 def draw_birb():
-    cnv_wall.create_oval(user_center[0], user_center[1], 
-                        user_center[0], user_center[1], 
-                        width=6, outline="red")
-    #cnv_wall.create_oval(birb_center[0], birb_center[1], 
+    cnv_wall.create_oval(user_center[0], user_center[1],
+                         user_center[0], user_center[1],
+                         width=6, outline="red")
+    # cnv_wall.create_oval(birb_center[0], birb_center[1],
     #                    birb_center[0], birb_center[1], 
     #                    width=8, outline="blue")
     # Beak
     for i in range(len(beak) - 1):
         cnv_wall.create_line(beak[i][0], beak[i][1], beak[i + 1][0], beak[i + 1][1],
-                            width=birb_width + 1)
+                             width=birb_width + 1)
 
     # Head
     for i in range(len(head)):
-        cnv_wall.create_oval(head[i][0], head[i][1], head[i][0], head[i][1], 
-                            width=birb_width)
-    
+        cnv_wall.create_oval(head[i][0], head[i][1], head[i][0], head[i][1],
+                             width=birb_width)
+
     # Body
     for i in range(len(body)):
-        cnv_wall.create_oval(body[i][0], body[i][1], body[i][0], body[i][1], 
-                            width=birb_width)
+        cnv_wall.create_oval(body[i][0], body[i][1], body[i][0], body[i][1],
+                             width=birb_width)
 
     # Tail
     for i in range(len(tail) - 1):
         cnv_wall.create_line(tail[i][0], tail[i][1], tail[i + 1][0], tail[i + 1][1],
-                            width=birb_width + 1)
+                             width=birb_width + 1)
 
     # Wing
     for i in range(len(tail) - 1):
         cnv_wall.create_line(wing[i][0], wing[i][1], wing[i + 1][0], wing[i + 1][1],
-                            width=birb_width + 1)
+                             width=birb_width + 1)
 
     # Paws
-    cnv_wall.create_line(paws[0][0], paws[0][1], paws[1][0], paws[1][1], 
-                        width=birb_width + 1)
-    cnv_wall.create_line(paws[2][0], paws[2][1], paws[3][0], paws[3][1], 
-                        width=birb_width + 1)
+    cnv_wall.create_line(paws[0][0], paws[0][1], paws[1][0], paws[1][1],
+                         width=birb_width + 1)
+    cnv_wall.create_line(paws[2][0], paws[2][1], paws[3][0], paws[3][1],
+                         width=birb_width + 1)
 
 
 def main():
